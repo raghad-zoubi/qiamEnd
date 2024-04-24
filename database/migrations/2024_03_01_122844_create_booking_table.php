c153a730ffb1626c2ef68c53655cef852a7c7509
raghad-zoubi <you@example.com>
2024-04-24 13:08:29 +0300

    16 edit

--- database/migrations/2024_03_01_122844_create_booking_table.php
+++ database/migrations/2024_03_01_122844_create_booking_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->foreignId("id_online_center")->constrained("online_centers")->cascadeOnDelete();
             $table->foreignId("id_user")->constrained("users")->cascadeOnDelete();
             $table->bigInteger("mark");
-            $table->boolean("status")->default('0');
+            $table->enum("status",["0","1"])->default("0");
+
 
             $table->timestamps();
         });

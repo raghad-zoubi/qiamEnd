8a2d7042a849fc8e7c5eb60c73f16bceed4265c4
raghad-zoubi <you@example.com>
2024-05-01 15:41:52 +0300

    24 edit

--- database/migrations/2024_03_01_091406_create_answers_table.php
+++ database/migrations/2024_03_01_091406_create_answers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("id_answer")->constrained("answers")->cascadeOnDelete();
+            $table->foreignId("id_option")->constrained("options")->cascadeOnDelete();
             $table->foreignId("id_user")->constrained("users")->cascadeOnDelete();
 
             $table->timestamps();
